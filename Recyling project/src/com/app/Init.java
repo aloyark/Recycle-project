cb2ae6587faf036f296010486d9e8b507c696be3
aloyark <thebox867@gmail.com>
2016-10-08 21:43:48 -0400

    Version 1.0

--- Recyling project/src/com/app/Init.java
+++ Recyling project/src/com/app/Init.java
@@ -1,7 +1,14 @@
+/*
+ * Author Xavier Yorks and Garnet Grimm
+ */
+
 package com.app;
+
+import java.io.IOException;
 
 public class Init {
 	public static void main(String[] args) {
 		Window window = new Window();
+		
 	}
 }
